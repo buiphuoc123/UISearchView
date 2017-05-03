9063ec73ce8b8f24439e866801b56adaa18a7d47
BuiPhuoc <buiphuoc.spkt@gmail.com>
2017-05-03 13:04:06 +0700

    Initial Commit

---
+++ UISearchController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UISearchController
+//
+//  Created by Cntt05 on 5/3/17.
+//  Copyright © 2017 Cntt05. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
